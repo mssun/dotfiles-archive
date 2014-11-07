c3532ebc1e3f8817cdfa2eae110a62b3e6557b28
Mingshen Sun <mssun@cse.cuhk.edu.hk>
2014-11-07 14:23:32 +0800

    add lazy-major-mode funmacro

--- .emacs.d/config/init-util.el
+++ .emacs.d/config/init-util.el
@@ -9,4 +9,10 @@
     `(eval-after-load ,feature
        '(progn ,@body))))
 
+(defmacro lazy-major-mode (pattern mode)
+  "Defines a new major-mode matched by PATTERN, installs MODE if necessary, and activates it."
+  `(add-to-list 'auto-mode-alist
+                '(,pattern . (lambda ()
+                             (require-package (quote ,mode))
+                             (,mode)))))
 (provide 'init-util)
