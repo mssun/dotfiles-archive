94807f55f8a0dd8f8d07e35bff27bb65ef339bf3
Mingshen Sun <mssun@cse.cuhk.edu.hk>
2014-11-07 23:16:17 +0800

    add evil-leader

--- .emacs.d/config/init-evil.el
+++ .emacs.d/config/init-evil.el
@@ -1,5 +1,21 @@
-(require-package 'evil)
 (setq evil-want-C-u-scroll t)
+(setq evil-search-module 'evil-search)
+(setq evil-magic 'very-magic)
+
+(require-package 'evil)
 (require 'evil)
+
+(require-package 'evil-leader)
+(global-evil-leader-mode t)
+(evil-leader/set-leader ",")
+(evil-leader/set-key
+  "e" 'find-file
+  "b" 'switch-to-buffer
+  "k" 'kill-buffer)
+
+(setq evilnc-hotkey-comment-operator "gc")
+(require-package 'evil-nerd-commenter)
+(require 'evil-nerd-commenter)
+
 (evil-mode t)
 (provide 'init-evil)
