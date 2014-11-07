22e926df1f91d2baa6487cbb494b9870db6e1428
Mingshen Sun <mssun@cse.cuhk.edu.hk>
2014-11-07 23:31:29 +0800

    move vim binding to init-bindings

---
+++ .emacs.d/config/init-bindings.el
@@ -0,0 +1,9 @@
+(after 'evil
+    (after "evil-leader-autoloads"
+	(evil-leader/set-leader ",")
+	(evil-leader/set-key
+	  "e" 'find-file
+	  "b" 'switch-to-buffer
+	  "k" 'kill-buffer
+	  "w" 'save-buffer
+	  "b d" 'kill-this-buffer)))
--- .emacs.d/config/init-evil.el
+++ .emacs.d/config/init-evil.el
@@ -7,11 +7,6 @@
 
 (require-package 'evil-leader)
 (global-evil-leader-mode t)
-(evil-leader/set-leader ",")
-(evil-leader/set-key
-  "e" 'find-file
-  "b" 'switch-to-buffer
-  "k" 'kill-buffer)
 
 (setq evilnc-hotkey-comment-operator "gc")
 (require-package 'evil-nerd-commenter)
