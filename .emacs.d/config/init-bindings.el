2344811d26ee7e002a92b9a978f9560e13a33fe6
Mingshen Sun <mssun@cse.cuhk.edu.hk>
2014-11-09 14:51:40 +0800

    add window movement bindings

--- .emacs.d/config/init-bindings.el
+++ .emacs.d/config/init-bindings.el
@@ -11,6 +11,13 @@
 	  "w" 'save-buffer))
     (after "company-autoloads"
       (define-key company-active-map (kbd "C-n") 'company-select-next)
-      (define-key company-active-map (kbd "C-p") 'company-select-previous)))
+      (define-key company-active-map (kbd "C-p") 'company-select-previous))
+
+    (global-set-key (kbd "C-w") 'evil-window-map)
+    (define-key evil-normal-state-map (kbd "C-h") 'evil-window-left)
+    (define-key evil-normal-state-map (kbd "C-j") 'evil-window-down)
+    (define-key evil-normal-state-map (kbd "C-k") 'evil-window-up)
+    (define-key evil-normal-state-map (kbd "C-l") 'evil-window-right)
+    )
 
 (provide 'init-bindings)
