e0f39ce36159dc384b3aeb9f64f5899edee94619
Mingshen Sun <mssun@cse.cuhk.edu.hk>
2014-11-18 19:45:54 +0800

    optimize company mode

--- .emacs.d/config/init-company.el
+++ .emacs.d/config/init-company.el
@@ -1,5 +1,16 @@
 (require-package 'company)
 (require 'company)
+
+(setq company-idle-delay 0.2)
+(setq company-minimum-prefix-length 1)
+(setq company-show-numbers t)
+(setq company-tooltip-limit 20)
+
+(setq company-global-modes
+      '(not
+        org-mode
+        latex-mode
+        tex-mode))
 (global-company-mode)
 
 (provide 'init-company)
