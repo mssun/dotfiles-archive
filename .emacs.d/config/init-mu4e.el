82aff81bab0a319e681688fc29d23d8d12184a02
Mingshen Sun <mssun@cse.cuhk.edu.hk>
2015-01-30 16:46:12 +0800

    Cleanup.

--- .emacs.d/config/init-mu4e.el
+++ .emacs.d/config/init-mu4e.el
@@ -13,40 +13,6 @@
 ;; don't save message to Sent Messages, Gmail/IMAP takes care of this
 (setq mu4e-sent-messages-behavior 'delete)
 
-;; (See the documentation for `mu4e-sent-messages-behavior' if you have
-;; additional non-Gmail addresses and want assign them different
-;; behavior.)
-
-;; setup some handy shortcuts
-;; you can quickly switch to your Inbox -- press ``ji''
-;; then, when you want archive some messages, move them to
-;; the 'All Mail' folder by pressing ``ma''.
-
-;; These following key bindings doesn't work. I still don't know why.
-;; (eval-after-load 'mu4e
-;;   '(progn
-;;      (evil-make-overriding-map mu4e-view-mode-map 'normal t)
-;;      (evil-make-overriding-map mu4e-main-mode-map 'normal t)
-;;      (evil-make-overriding-map mu4e-headers-mode-map 'normal t)
-;; 
-;;      (evil-add-hjkl-bindings mu4e-view-mode-map 'normal
-;;        "J" 'mu4e~headers-jump-to-maildir
-;;        "j" 'evil-next-line
-;;        "C" 'mu4e-compose-new
-;;        "o" 'mu4e-view-message
-;;        "Q" 'mu4e-raw-view-quit-buffer)
-;; 
-;;      (evil-add-hjkl-bindings mu4e-header-mode-map 'normal
-;;        "J" 'mu4e~headers-jump-to-maildir
-;;        "j" 'evil-next-line
-;;        "C" 'mu4e-compose-new
-;;        "o" 'mu4e-view-message)
-;; 
-;;      (evil-add-hjkl-bindings mu4e-main-mode-map 'normal
-;;        "J" 'mu4e~headers-jump-to-maildir
-;;        "j" 'evil-next-line
-;;        "RET" 'mu4e-view-message)))
-
 (setq mu4e-maildir-shortcuts
     '( ("/gmail/INBOX"  . ?i)
        ("/gmail/sent"   . ?s)
@@ -55,7 +21,7 @@
 
 ;; allow for updating mail using 'U' in the main view:
 (setq mu4e-get-mail-command "offlineimap")
-(setq mu4e-update-interval 150)
+(setq mu4e-update-interval 60)
 
 ;; something about ourselves
 (setq
@@ -71,21 +37,12 @@
 ;; package 'gnutls-bin' in Debian/Ubuntu
 
 (require 'smtpmail)
-;; (setq message-send-mail-function 'smtpmail-send-it
-;;    starttls-use-gnutls t
-;;    smtpmail-starttls-credentials '(("smtp.gmail.com" 587 nil nil))
-;;    smtpmail-auth-credentials
-;;      '(("smtp.gmail.com" 587 "USERNAME@gmail.com" nil))
-;;    smtpmail-default-smtp-server "smtp.gmail.com"
-;;    smtpmail-smtp-server "smtp.gmail.com"
-;;    smtpmail-smtp-service 587)
-
-;; alternatively, for emacs-24 you can use:
 (setq message-send-mail-function 'smtpmail-send-it
      smtpmail-stream-type 'starttls
      smtpmail-default-smtp-server "smtp.gmail.com"
      smtpmail-smtp-server "smtp.gmail.com"
      smtpmail-smtp-service 587)
+
 (add-hook 'message-send-hook
           (lambda ()
             (unless (yes-or-no-p "Sure you want to send this?")
@@ -93,7 +50,11 @@
 
 ;; don't keep message buffers around
 (setq message-kill-buffer-on-exit t)
-
 (setq mu4e-hide-index-messages t)
+(setq mu4e-attachment-dir "~/Downloads")
+(setq mu4e-view-show-images t
+      mu4e-view-image-max-width 700)
+
+(setq mu4e-html2text-command "w3m -dump -cols 110 -T text/html")
 
 (provide 'init-mu4e)
