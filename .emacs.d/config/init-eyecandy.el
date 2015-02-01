5bca1d88c2b2a5b59690b95d2fb5f58030364a67
Mingshen Sun <mssun@cse.cuhk.edu.hk>
2015-02-01 23:38:04 +0800

    Update eyecandy.

--- .emacs.d/config/init-eyecandy.el
+++ .emacs.d/config/init-eyecandy.el
@@ -1,6 +1,31 @@
 ; (setq show-paren-delay 0)
 (show-paren-mode t)
 
+(line-number-mode t)
+(column-number-mode t)
+
+;; (require-package 'diminish)
+;; (diminish 'visual-line-mode)
+;; (after 'undo-tree (diminish 'undo-tree-mode))
+;; (after 'projectile (diminish 'projectile-mode))
+;; (after 'yasnippet (diminish 'yas-minor-mode))
+;; (after 'smartparens (diminish 'smartparens-mode))
+
+(require-package 'rich-minority)
+(require 'rich-minority)
+(add-to-list 'rm-whitelist nil t)
+(rich-minority-mode 1)
+
+(require-package 'smart-mode-line)
+(setq sml/show-client t)
+(setq sml/show-eol t)
+(setq sml/show-frame-identification t)
+(setq sml/no-confirm-load-theme t)
+(setq sml/shorten-directory t)
+(setq sml/shorten-modes t)
+(sml/setup)
+(sml/apply-theme 'respectful)
+
 ; (setq display-time-day-and-date t
 ;       display-time-24hr-format t
 ;       display-time-interval 10
