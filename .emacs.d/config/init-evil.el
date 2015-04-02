9dbf0b61dc2ac386a851dde7bc63d9789abe20b4
Mingshen Sun <mssun@cse.cuhk.edu.hk>
2015-04-02 15:26:15 +0800

    Add some plugin of evil mode.

--- .emacs.d/config/init-evil.el
+++ .emacs.d/config/init-evil.el
@@ -5,16 +5,27 @@
 (require-package 'evil)
 (require 'evil)
 
+(unless (display-graphic-p)
+  (evil-esc-mode))
+
 (setq evil-cross-lines t)
 
 (require-package 'evil-leader)
 (global-evil-leader-mode t)
 
+(require-package 'evil-matchit)
+(global-evil-matchit-mode t)
+
 (setq evilnc-hotkey-comment-operator "gc")
 (require-package 'evil-nerd-commenter)
 (require 'evil-nerd-commenter)
 
-(evil-mode t)
+(require-package 'evil-surround)
+(global-evil-surround-mode t)
 
+(require-package 'evil-visualstar)
+(global-evil-visualstar-mode t)
+
+(evil-mode t)
 
 (provide 'init-evil)
