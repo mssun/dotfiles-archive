e56e55803e742a1cfb1cc30a03d2b63e1f2adb00
Mingshen Sun <mssun@cse.cuhk.edu.hk>
2015-04-09 17:54:47 +0800

    Update company configuration, make it faster.

--- .emacs.d/config/init-company.el
+++ .emacs.d/config/init-company.el
@@ -1,10 +1,18 @@
 (require-package 'company)
 (require 'company)
 
-(setq company-idle-delay 0.2)
-(setq company-minimum-prefix-length 3)
+(setq company-idle-delay 0.1)
+(setq company-minimum-prefix-length 2)
 (setq company-show-numbers t)
 (setq company-tooltip-limit 20)
+
+(setq company-dabbrev-downcase nil)
+(setq company-dabbrev-ignore-case t)
+
+(setq company-dabbrev-code-ignore-case t)
+(setq company-dabbrev-code-everywhere t)
+
+(setq company-etags-ignore-case t)
 
 (setq company-global-modes
       '(not
