f5f1c00c6eafc2a74d35cf589291ccae1bca4199
Mingshen Sun <mssun@cse.cuhk.edu.hk>
2015-04-09 17:55:21 +0800

    Update smartparens configs.

--- .emacs.d/config/init-smartparens.el
+++ .emacs.d/config/init-smartparens.el
@@ -1,3 +1,8 @@
 (require-package 'smartparens)
+(require 'smartparens-config)
+
+(setq sp-autoescape-string-quote nil)
+(setq sp-autoinsert-quote-if-followed-by-closing-pair nil)
+(sp-use-smartparens-bindings)
 (smartparens-global-mode t)
 (provide 'init-smartparens)
